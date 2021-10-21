888f67375482bfe24b4fe5df933c38b923af40e7
Vasil Kachamakov <73233479+vasko01@users.noreply.github.com>
2021-10-21 15:18:56 +0300

    Added task solutions

--- week01/tasks/task2.cpp
+++ week01/tasks/task2.cpp
@@ -5,17 +5,24 @@ using namespace std;
 int main()
 {
     int num;
+    
     cout << "Enter a 3-digit number: ";
     cin >> num;
-    cout << "The digits of the number are:" << endl;
+    
     int hundreds = num / 100;
     int tens = (num / 10) % 10;
     int units = num % 10;
-    int sumOfDigits = hundreds + tens + units;
-    int multOfDigits = hundreds * tens * units;
+    
+    cout << "The digits of the number are:" << endl;
     cout << hundreds << endl;
     cout << tens << endl;
     cout << units << endl;
+    
+    int sumOfDigits = hundreds + tens + units;
     cout << "Sum of digits: " << sumOfDigits << endl;
+    
+    int multOfDigits = hundreds * tens * units;
     cout << "Multiplication of digits: " << multOfDigits << endl; 
+    
+    return 0;
 }
