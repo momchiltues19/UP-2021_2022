5d0ac6bfe9ae5fd3327b8e0d26182f1caab28652
MarianaT02 <57329701+MarianaT02@users.noreply.github.com>
2021-10-22 16:34:24 +0300

    Create task1.cpp

---
+++ week02/tasks/Mariana Todorova/task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+  double arg1, arg2;
+  char symbol;
+  
+  cout << "Number 1 is: " << endl;
+  cin >> arg1;
+  cout << "Number 2 is: " << endl;
+  cin >> arg2;
+  cout << "The operation you use is: " << endl;
+  cin >> symbol;
+  
+  switch(symbol){
+    case '+':{ cout << arg1 + arg2; break; }
+    case '-':{ cout << arg1 - arg2; break; }
+    case '*':{ cout << arg1 * arg2; break; }
+    case '/':{ if (arg2 == 0) cout<< "Error!";
+              else cout << arg1 / arg2; break;}
+    defaut: cout << "The operator is wrong"; break;
+  }
+  
+  return 0;
+}
